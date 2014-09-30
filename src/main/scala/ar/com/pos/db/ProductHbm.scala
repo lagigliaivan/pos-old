0c29660388e8b8146ff0397710ef043a3c957c04
unknown <ilagiglia@ZAR03-PROJ-176.ARRS.ARRISI.com>
2014-09-30 11:57:01 -0300

    everything compiles

--- src/main/scala/ar/com/pos/db/ProductHbm.scala
+++ src/main/scala/ar/com/pos/db/ProductHbm.scala
@@ -4,22 +4,21 @@ import java.util
 
 class ProductHbm {
 	
-	private var _idproduct: String = _;
-	private var _description: String = _;
-	private var _stock: Integer = 0;
-	private var _price: Float = 0;
-	private var _sales = HashSet[SaleHbm]();
+	private var _idproduct: String = _
+	private var _description: String = _
+	private var _stock: Integer = 0
+	private var _price: Float = 0
+	private var _sales = new util.HashSet[SaleHbm]()
 
-	def getIdproduct = _idproduct;
-	def getDescription = _description;
-	def getStock = _stock;
-	def getPrice = _price;
-	def getSales = _sales;
+	def getIdproduct = _idproduct
+	def getDescription = _description
+	def getStock = _stock
+	def getPrice = _price
+	def getSales = _sales
 	
-	def setIdproduct (value:String):Unit = _idproduct = value;
-	def setDescription (value:String):Unit = _description = value;
-	def setStock (value:Integer):Unit = _stock = value;
-	def setPrice (value:Float):Unit = _price = value;
-	def setSales (value:util.HashSet[SaleHbm]):Unit = _sales = value;
-
+	def setIdproduct (value:String):Unit = _idproduct = value
+	def setDescription (value:String):Unit = _description = value
+	def setStock (value:Integer):Unit = _stock = value
+	def setPrice (value:Float):Unit = _price = value
+	def setSales (value:util.HashSet[SaleHbm]):Unit = _sales = value
 }
